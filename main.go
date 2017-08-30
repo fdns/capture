feee84266ea109aa9634d8aefc5092317e2eaa3d
Felipe Espinoza <yayofelipe@gmail.com>
2017-08-30 15:27:40 -0300

    Testing a culmnar insertion of the data

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"container/list"
-	"database/sql"
+	"database/sql/driver"
 	"flag"
 	"fmt"
 	"log"
@@ -10,9 +9,11 @@ import (
 	"time"
 	"runtime/pprof"
 
-	_ "github.com/kshvakov/clickhouse"
 	"os"
 	"runtime"
+	_ "github.com/kshvakov/clickhouse"
+	data "github.com/kshvakov/clickhouse/lib/data"
+	"github.com/kshvakov/clickhouse"
 	"strings"
 )
 
@@ -25,7 +26,7 @@ var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 
-func connectClickhouse(exiting chan bool) *sql.DB {
+func connectClickhouse(exiting chan bool) clickhouse.Clickhouse {
 	tick := time.NewTimer(5 * time.Second)
 	defer tick.Stop()
 	for {
@@ -34,12 +35,13 @@ func connectClickhouse(exiting chan bool) *sql.DB {
 			// When exiting, return inmediatly
 			return nil
 		case <-tick.C:
-			connection, err := sql.Open("clickhouse", "tcp://172.30.65.172:9000?username=&compress=true&debug=false")
+			connection, err := clickhouse.OpenDirect("tcp://172.30.65.172:9000?username=&compress=true&debug=false")
 			if err != nil {
 				log.Println(err)
 				continue
 			}
-			_, err = connection.Exec(`
+			{
+				stmt, _ := connection.Prepare(`
 			CREATE TABLE IF NOT EXISTS DNS_LOG (
 				DnsDate Date,
 				timestamp DateTime,
@@ -50,22 +52,28 @@ func connectClickhouse(exiting chan bool) *sql.DB {
 				Type UInt16,
 				ResponceCode UInt8,
 				Question String,
-				SourceIPMask FixedString(15),
+				SourceIPMask String,
 				Size UInt16
 			) engine=MergeTree(DnsDate, (timestamp, Question, Protocol), 8192)
 			`)
-			if err != nil {
-				log.Println(err)
-				continue
+				if _, err := stmt.Exec([]driver.Value{}); err != nil {
+					log.Println(err)
+					continue
+				}
+				connection.Commit()
 			}
-			_, err = connection.Exec(`
-			CREATE MATERIALIZED VIEW IF NOT EXISTS DNS_DOMAIN_COUNT
-		  	ENGINE=SummingMergeTree(DnsDate, (t, Question), 8192, c) AS
-		  	SELECT DnsDate, toStartOfMinute(timestamp) as t, Question, count(*) as c FROM DNS_LOG GROUP BY DnsDate, t, Question
-			`)
-			if err != nil {
-				log.Println(err)
-				continue
+			{
+				stmt, _ := connection.Prepare(`
+				CREATE MATERIALIZED VIEW IF NOT EXISTS DNS_DOMAIN_COUNT
+				ENGINE=SummingMergeTree(DnsDate, (t, Question), 8192, c) AS
+				SELECT DnsDate, toStartOfMinute(timestamp) as t, Question, count(*) as c FROM DNS_LOG GROUP BY DnsDate, t, Question
+				`)
+
+				if _, err := stmt.Exec([]driver.Value{}); err != nil {
+					log.Println(err)
+					continue
+				}
+				connection.Commit()
 			}
 			return connection
 		}
@@ -77,67 +85,106 @@ func output(resultChannel chan DnsResult, exiting chan bool, wg *sync.WaitGroup)
 	defer wg.Done()
 
 	connect := connectClickhouse(exiting)
-	batch := list.New()
+	batch := make([]DnsResult, 0, 200000)
 
 	ticker := time.Tick(time.Second)
 	for {
 		select {
 		case data := <-resultChannel:
-			batch.PushBack(data)
+			batch = append(batch, data)
 		case <-ticker:
 			if err := SendData(connect, batch, exiting); err != nil {
 				log.Println(err)
 				connect = connectClickhouse(exiting)
 			}
+			batch = make([]DnsResult, 0, 200000)
 		case <-exiting:
 			return
 		}
 	}
 }
 
-func SendData(connect *sql.DB, batch *list.List, exiting chan bool) error {
-	if batch.Len() == 0 {
+func min(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func SendData(connect clickhouse.Clickhouse, batch []DnsResult, exiting chan bool) error {
+	if len(batch) == 0 {
 		return nil
 	}
+	fmt.Println(len(batch))
+
 	// Return if the connection is null, we are exiting
 	if connect == nil {
 		return nil
 	}
-	tx, err := connect.Begin()
+	_, err := connect.Begin()
 	if err != nil {
 		return err
 	}
-	stmt, err := tx.Prepare("INSERT INTO DNS_LOG (DnsDate, timestamp, Protocol, QR, OpCode, Class, Type, ResponceCode, Question, SourceIPMask, Size) VALUES(?,?,?,?,?,?,?,?,?,?)")
+
+	_, err = connect.Prepare("INSERT INTO DNS_LOG (DnsDate, timestamp, Protocol, QR, OpCode, Class, Type, ResponceCode, Question, SourceIPMask, Size) VALUES(?,?,?,?,?,?,?,?,?,?,?)")
 	if err != nil {
 		return err
 	}
-	fmt.Println(batch.Len())
-	for iter := batch.Front(); iter != nil; iter = iter.Next() {
-		item := iter.Value.(DnsResult)
-		srcIpMask := strings.Split(item.SrcIP, ".")[0] + ".0.0.0"
-		for _, dnsQuestion := range item.Dns.Questions {
-			if _, err := stmt.Exec(item.timestamp,
-				item.timestamp,
-				item.Protocol,
-				item.Dns.QR,
-				int(item.Dns.OpCode),
-				int(dnsQuestion.Class),
-				int(dnsQuestion.Type),
-				int(item.Dns.ResponseCode),
-				string(dnsQuestion.Name),
-				srcIpMask,
-				item.PacketLength); err != nil {
-				if err != nil {
-					return err
+
+	block, err := connect.Block()
+	if err != nil {
+		return err
+	}
+
+	blocks := []*data.Block{block, block.Copy(), block.Copy()}
+
+	count := len(blocks)
+	var wg sync.WaitGroup
+	wg.Add(len(blocks))
+	for i := range blocks {
+		b := blocks[i]
+		start := i*(len(batch))/count
+		end := min((i+1)*(len(batch))/count, len(batch))
+
+		go func() {
+			defer wg.Done()
+			b.Reserve()
+			b.NumRows = uint64(end - start)
+			for k := start; k < end; k++ {
+				if len(batch[k].Dns.Questions) == 0 {
+					b.NumRows -= 1
+				}
+				for _, dnsQuery := range batch[k].Dns.Questions {
+					srcIpMask := strings.Split(batch[k].SrcIP, ".")[0] + ".0.0.0"
+					b.WriteDate(0, batch[k].timestamp)
+					b.WriteDateTime(1, batch[k].timestamp)
+					b.WriteString(2, (batch[k].Protocol))
+					QR := uint8(0)
+					if batch[k].Dns.QR {
+						QR = 1
+					}
+					b.WriteUInt8(3, QR)
+					b.WriteUInt8(4, uint8(batch[k].Dns.OpCode))
+					b.WriteUInt16(5, uint16(dnsQuery.Class))
+					b.WriteUInt16(6, uint16(dnsQuery.Type))
+					b.WriteUInt8(7, uint8(batch[k].Dns.ResponseCode))
+					b.WriteString(8, string(dnsQuery.Name))
+					b.WriteString(9, srcIpMask)
+					b.WriteUInt16(10, batch[k].PacketLength)
+					break
 				}
 			}
-		}
+			if err := connect.WriteBlock(b); err != nil {
+				log.Fatal(err)
+			}
+		}()
 	}
-	err = tx.Commit()
-	if err != nil {
-		return err
+
+	wg.Wait()
+	if err := connect.Commit(); err != nil {
+		log.Fatal(err)
 	}
-	batch.Init()
+
 	return nil
 }
 
